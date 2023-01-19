df57c495c34d7979ac35d1605ca9918f676d3812
AlbinHall <albin-hall1@hotmail.com>
2023-01-19 17:47:36 +0000

    add function for selecting the country from dropdownbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,24 @@
-<<<<<<< HEAD
-// API endpoint for getting exchange rates
-const API_ENDPOINT = "https://api.exchangerate-api.com/v4/latest";
 
-// Function to get the exchange rate
-function getExchangeRate(fromCurrency, toCurrency) {
-    return fetch(`${API_ENDPOINT}/${fromCurrency}`)
-        .then(response => response.json())
-        .then(data => data.rates[toCurrency]);
-}
-
-// Function to convert the currency
-function convertCurrency(amount, fromCurrency, toCurrency) {
-    return getExchangeRate(fromCurrency, toCurrency).then(rate => amount * rate);
-}
-
-// Function to handle the form submission
-function handleFormSubmit(event) {
-    event.preventDefault();
-    const amount = document.getElementById("amount").value;
-    const fromCurrency = document.getElementById("fromCurrency").value;
-    const toCurrency = document.getElementById("toCurrency").value;
-
-    convertCurrency(amount, fromCurrency, toCurrency)
-        .then(convertedAmount => {
-            document.getElementById("result").innerHTML = `${amount} ${fromCurrency} is equal to ${convertedAmount} ${toCurrency}`;
-        })
-        .catch(error => console.error(error));
-}
-
-// Add an event listener to the form
-document.getElementById("conversion-form").addEventListener("submit", handleFormSubmit);
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
-  });
-=======
-
+// function to calc and convert
 function convertCurrency() {
-  // Where the user selects country
-  let fromCountry = document.getElementById("fromCountry").value;
-
-  // How much money the user has to spend
+  // users current amount of money
   let money = document.getElementById("money").value;
 
-  // where the user wants to go
-  let toCountry = document.getElementById("toCountry").value;
+  // where user is located
+  let fromCountry = fromCountrySelect.value;
+
+  // get users location
+  let toCountry = toCountrySelect.value;
 
   // Use API to retrieve exchange rate between countries
-  fetch(`https://api.exchangerate-api.com/v4/latest/${fromCountry}`)
+  fetch(`https://api.exchangerate-api.com/v4/latest/${fromCountry}?apikey=your_api_key`)
     .then(response => response.json())
     .then(data => {
-      // calculation
+      // Calculation
       let exchangeRate = data.rates[toCountry];
       let travelMoney = money * exchangeRate;
 
-      // Display result to user
+      // result
       let result = `You would have ${travelMoney} ${toCountry} in ${toCountry}.`;
       document.getElementById("result").innerHTML = result;
     })
