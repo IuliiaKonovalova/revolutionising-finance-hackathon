f392b87139564bdfb920ee984e1613d71710bbe4
AVTpepper <Alexander.tastad@hotmail.com>
2023-01-19 18:40:33 +0000

    Javascript functionality and trying to figure out the html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,11 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 00491b4 (add styling to form html)
+=======
+>>>>>>> 093b6f9 (Javascript functionality and trying to figure out the html)
 
 
 
@@ -172,5 +175,49 @@ const calculateFinal = function() {
 buttonCalculate.addEventListener("click", calculateFinal);
 
 // test
+<<<<<<< HEAD
 >>>>>>> 7ed1e97 (Add event listener on clalculate button)
+<<<<<<< HEAD
 >>>>>>> ba104f7 (Add event listener on clalculate button)
+=======
+=======
+=======
+// API endpoint for getting exchange rates
+const API_ENDPOINT = "https://api.exchangerate-api.com/v4/latest";
+
+// Function to get the exchange rate
+function getExchangeRate(fromCurrency, toCurrency) {
+    return fetch(`${API_ENDPOINT}/${fromCurrency}`)
+        .then(response => response.json())
+        .then(data => data.rates[toCurrency]);
+}
+
+// Function to convert the currency
+function convertCurrency(amount, fromCurrency, toCurrency) {
+    return getExchangeRate(fromCurrency, toCurrency).then(rate => amount * rate);
+}
+
+// Function to handle the form submission
+function handleFormSubmit(event) {
+    event.preventDefault();
+    const amount = document.getElementById("amount").value;
+    const fromCurrency = document.getElementById("fromCurrency").value;
+    const toCurrency = document.getElementById("toCurrency").value;
+
+    convertCurrency(amount, fromCurrency, toCurrency)
+        .then(convertedAmount => {
+            document.getElementById("result").innerHTML = `${amount} ${fromCurrency} is equal to ${convertedAmount} ${toCurrency}`;
+        })
+        .catch(error => console.error(error));
+}
+
+// Add an event listener to the form
+document.getElementById("conversion-form").addEventListener("submit", handleFormSubmit);
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems, options);
+  });
+>>>>>>> 6cc5b72 (Javascript functionality and trying to figure out the html)
+>>>>>>> d371c6e (Javascript functionality and trying to figure out the html)
+>>>>>>> 093b6f9 (Javascript functionality and trying to figure out the html)
