3bdcc4bb3665994ac25a9127478662e1a92634ef
AlbinHall <albin-hall1@hotmail.com>
2023-01-19 18:06:40 +0000

    trying to solve problem

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+let fromCountrySelect = document.getElementById("fromCountry");
+let toCountrySelect = document.getElementById("toCountry");
+// gets the api for countries
+fetch("https://api.exchangerate-api.com/v4/countries?apikey=your_api_key")
+  .then(response => response.json())
+  .then(data => {
+    let countries = data.countries;
+    for (let country in countries) {
+      let option = document.createElement("option");
+      option.value = country;
+      option.text = countries[country];
+      fromCountrySelect.add(option);
+      toCountrySelect.add(option.cloneNode(true));
+    }
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
 // function to calc and convert
 function convertCurrency() {
@@ -11,6 +29,7 @@ function convertCurrency() {
   let toCountry = toCountrySelect.value;
 
   // Use API to retrieve exchange rate between countries
+  // the api is non active just a dummy 
   fetch(`https://api.exchangerate-api.com/v4/latest/${fromCountry}?apikey=your_api_key`)
     .then(response => response.json())
     .then(data => {
