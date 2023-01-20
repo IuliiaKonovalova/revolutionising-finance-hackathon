fa0ca4850638d9f835727280250e03526b7d6a3c
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-01-21 04:18:47 +0700

    Add call to retrieve data for longiture and latitude

--- assets/js/api_key_travel.js
+++ assets/js/api_key_travel.js
@@ -7,7 +7,40 @@ const amadeusInput = document.getElementById("amadeus-input");
 const amadeusButton = document.getElementById("amadeus-button");
 const amadeusResult = document.getElementById("amadeus-result");
 
+const longlatApiKey = "ChURwrLnW6pGX0zk2OWBRg==14Dkv21DHTJioryM";
 
+// get latitude and longitude from the input field from geonames
+
+let country = "Germany";
+let city = "London";
+let latitude;
+let longitude;
+
+const getLongLat = async () => {
+  const longlatUrl = `https://api.api-ninjas.com/v1/city?name=${city}`;
+  const longlatHeaders = {
+    "X-Api-Key": longlatApiKey,
+  };
+  const longlatResponse = await fetch(longlatUrl, {
+    method: "GET",
+    headers: longlatHeaders,
+  });
+  const longlatData = await longlatResponse.json();
+  console.log(longlatData);
+  console.log(longlatData[0].latitude);
+  console.log(longlatData[0].longitude);
+  // check the type of longlatData[0].latitude
+  console.log(typeof longlatData[0].latitude);
+  // convert to integer
+  latitude = longlatData[0].latitude;
+  longitude = longlatData[0].longitude;
+  // get latitiude and longitude from the 
+
+
+  return longlatData;
+};
+
+const longlat = getLongLat();
 
 
 
@@ -45,7 +78,11 @@ const locationPois = async (lat, long) => {
 
 const locations = locationPois(51.5072, -0.1275);
 
-
+// Promise to get the latitude and longitude
+getLongLat().then(() => {
+  locationPois(latitude, longitude);
+});
+console.log(locations);
 
 // create a fetch request to the API
 const amadeusFetch = function() {
