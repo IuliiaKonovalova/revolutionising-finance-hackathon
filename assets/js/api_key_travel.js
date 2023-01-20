e16947b378c64a5aeb618afcc608e03963adb1b7
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-01-21 04:06:00 +0700

    Set up amadues access token

---
+++ assets/js/api_key_travel.js
@@ -0,0 +1,26 @@
+const apiKey = "dc25Xb49AgAGTwWdnMICd4AbhXx3dAvh";
+const apiSecret = "6lZAsTw9hqhPPvgv";
+const grantType = "client_credentials";
+
+
+
+
+
+
+const obtainToken = async () => {
+  const amadeusUrl = "https://test.api.amadeus.com/v1/security/oauth2/token";
+  const amadeusHeaders = {
+      "Content-Type": "application/x-www-form-urlencoded",
+  };
+  const amadeusBody = `grant_type=client_credentials&client_id=${apiKey}&client_secret=${apiSecret}`;
+  const amadeusResponse = await fetch(amadeusUrl, {
+      method: "POST",
+      headers: amadeusHeaders,
+      body: amadeusBody,
+  });
+  const amadeusData = await amadeusResponse.json();
+  console.log(amadeusData.access_token);
+  return amadeusData.access_token;
+};
+
+
