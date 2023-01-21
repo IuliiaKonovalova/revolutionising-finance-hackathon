a158994fcaec75b4d5e0047e49717a3f046043eb
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-01-21 13:37:04 +0000

    Create js code to obtain data from API

---
+++ assets/js/currency.js
@@ -0,0 +1,21 @@
+const APIKey = '4e21e51b0599ed099d6b98c4'
+
+document.querySelector('#form').onsubmit = () => {
+    const base = document.querySelector('#fromCurrency').value;
+    fetch(`https://v6.exchangerate-api.com/v6/${APIKey}/latest/USD`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            const amount = document.querySelector("#amount").value;
+            const currencyTo = document.querySelector("#toCurrency").value;
+            const rate = data.rates[currencyTo];
+            function convert() {
+                return amount * rate;
+            }
+            document.querySelector('#results').innerHTML = `${amount} ${base.toUpperCase()} equal to ${currencyTo} ${convert().toFixed(2)}`;
+        })
+        .catch((error) => {
+            console.log("Error: ", error);
+        });
+    return false;
+}
