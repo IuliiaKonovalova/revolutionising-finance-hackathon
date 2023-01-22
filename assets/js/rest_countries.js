4f1253a6a9d0781e8dc19ab0b85f37fff858d07d
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-01-23 00:40:38 +0700

    Resolve merge conflict

--- assets/js/rest_countries.js
+++ assets/js/rest_countries.js
@@ -1,5 +1,7 @@
 
 
+
+
 const countrySelect = document.getElementById("countries-select");
 const selectDestination = document.getElementById("destination-select");
 const currencyFrom = document.getElementById("currency-from");
@@ -13,8 +15,8 @@ let countries_data = [];
 const fetchCountries = async () => {
   const response = await fetch("https://restcountries.com/v3.1/all");
   const data = await response.json();
-  countries_data = [...data];
-  return countries_data;
+  countries_data = [...data]
+  return countries_data
 };
 
 fetchCountries().then((data) => {
