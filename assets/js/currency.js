a69545c05dbea3891a92c7be9ad11f05e2588fdb
Rsrbai <rsrbailey@gmail.com>
2023-01-22 18:27:25 +0000

    Add JShint validation

--- assets/js/currency.js
+++ assets/js/currency.js
@@ -2,8 +2,11 @@ const APIKey = '4e21e51b0599ed099d6b98c4';
 const currencyFromCountry = document.getElementById("currency-from");
 
 //Exchange Rate Function
+
+//Exchange Rate Function
 document.querySelector('#form').onsubmit = () => {
     const currencyFromCountryValue = currencyFromCountry.innerHTML;
+    const currencyToCountryValue = document.querySelector("#currency-to").innerHTML; 
     const currencyToCountryValue = document.querySelector("#currency-to").innerHTML; 
     fetch(`https://v6.exchangerate-api.com/v6/${APIKey}/latest/${currencyFromCountryValue}`)
         .then((response) => response.json())
@@ -11,16 +14,13 @@ document.querySelector('#form').onsubmit = () => {
             const amount = document.querySelector("#money").value;
             const rate = data.conversion_rates[currencyToCountryValue];
             //Function to calculate the value of money in destination country
-            //Function to calculate the value of money in destination country
             function convert() {
                 return amount * rate;
             }
             document.querySelector('#result').innerHTML = `${amount} ${currencyFromCountryValue} is equal to ${currencyToCountryValue} ${convert().toFixed(2)}`;
-            
         })
         .catch((error) => {
             console.log("Error: ", error);
         });
     return false;
-};
 };
